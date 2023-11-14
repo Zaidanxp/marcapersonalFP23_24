4ad5f2d34a67cf74b81ac674708c4cd8f47f312c
Zaidanxp <3720290@alu.murciaeduca.es>
2023-11-14 09:50:40 +0100

    Creación del partial mpfooter que es sección del partial footer

---
+++ resources/views/dopetrope/partials/footer/mpfooter.blade.php
@@ -0,0 +1,14 @@
+<section>
+    <header>
+        <h2>Información sobre Marca Personal F.P.</h2>
+    </header>
+    <a href="#" class="image featured"><img src="{{ asset('/dopetrope/images/pic10.jpg') }}" alt="" /></a>
+    <p>
+        Web marcapersonalFP.es desarrollada como proyecto por un grupo de estudiantes de 2DAW CarlosIII
+    </p>
+    <footer>
+        <ul class="actions">
+            <li><a href="#" class="button">Más info</a></li>
+        </ul>
+    </footer>
+</section>
